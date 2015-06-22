5ad74f2467df82cae4cb0599b27ff59ec3d48e06
Guillaume Perrin <gperrin01@gmail.com>
2015-06-22 13:58:47 +0100

    finished file and folder structure (npm installs, models/seeds, JS, stylesheets, views)

--- models.js
+++ models.js
@@ -1,1 +1,25 @@
-models!
+// put our models here
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/moodsdb"); // random moodsdb title, should i change?
+
+// defining the blueprint for Post and Comment 
+var PostSchema = new mongoose.Schema({
+	title: {type: String, default: ''},
+	author: {type: String, default: ''},
+	text: {type: String, default: ''},
+	comments: {type: Array, default: []}
+});
+var CommentSchema = new mongoose.Schema({
+	content: {type: String, default: ''},
+	author: {type: String, default: ''},
+});
+
+
+
+// creating a mongoose Food model to allow us to instantiate new food documents;
+var Post = mongoose.model('Post', PostSchema);
+var Comment = mongoose.model('Comment', CommentSchema);
+
+// making food object available to other enviroments
+module.exports.Post = Post;
+module.exports.Comment = Comment;
