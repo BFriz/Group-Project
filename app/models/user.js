2cfb007f877bc66b27050a468ace75a9556b1eb9
Guillaume Perrin <gperrin01@gmail.com>
2015-06-22 20:24:24 +0100

    adding the likes arrays on the user file

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,7 @@
 // load the things we need
 var mongoose = require('mongoose');
 var bcrypt   = require('bcrypt-nodejs');
+mongoose.connect("mongodb://localhost/moodsdb"); // random moodsdb title, should i change?
 
 // define the schema for our user model
 var userSchema = mongoose.Schema({
@@ -26,7 +27,25 @@ var userSchema = mongoose.Schema({
         token        : String,
         email        : String,
         name         : String
-    }
+    },
+    display_name: {type: String, required: true},
+    dob: {type: String, required: true, required: true},  // or "date" type?  see note on DOB vs Set age
+    gender: {type: String, required: true, required: true},
+  // looking_for: {type: String, default: ''}, this will be next step as it requires validation with gender
+    profile_pic_url: {type: String, default: '' },
+    all_pics_url: {type: Array, default: [] },
+
+   // dynamic attributes
+    mood: {type: String, default: ''}, // see note on validations on the mood
+    location: {type: String, default: ''}, // TBD if it is String
+
+    // uncertainty on the below, need to double check the self-referencing
+    likes: [],
+    dislikes: [],
+    matches: []
+    // matches: [{
+    //   type: mongoose.Schema.Types.ObjectId, ref: 'User'
+    // }],
 
 });
 
@@ -43,3 +62,19 @@ userSchema.methods.validPassword = function(password) {
 
 // create the model for users and expose it to our app
 module.exports = mongoose.model('User', userSchema);
+
+
+
+// // NOTES - NOTES - NOTES - NOTES NOTES - NOTES
+
+// // Dates - see http://mongoosejs.com/docs/schematypes.html
+// // Built-in Date methods are not hooked into the mongoose change tracking logic which in English means
+// // that if you use a Date in your document and modify it with a method like setMonth(), mongoose will be 
+// // unaware of this change and doc.save() will not persist this modification. If you must modify Date types 
+// // using built-in methods, tell mongoose about the change with doc.markModified('pathToYourDate') before saving.
+
+// // need to Add validations on the mood: can only be within a pre-selected set 
+// // -> AVOID anyone fiddling with the html source and creating a new mood
+
+// // where to put the password?
+
