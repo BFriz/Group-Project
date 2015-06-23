59f3709007e4f6874b16b22708ff0b28cc496a62
Guillaume Perrin <gperrin01@gmail.com>
2015-06-23 22:44:45 +0100

    clicking yes or no loads another profile - for the moment it doesnt add anything to the likes/dislikes arrays as we dont know yet how to access the current user

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,31 @@
 var View = View || {};
 var User = User || {};
 
+
+// ****** VIEW FUNCTIONS *****
+
 View = {
 
 	eventListeners: function() {
 
-		$( "#mood_menu a" ).click(function(event) {
+		$( "#mood_menu a" ).on('click', function(event) {
 	    console.log("button clicked!")
 	    event.preventDefault();
 	  });
+
+	  $('#left_panel').on('click', '#yes_button', function(event){ 
+	  	event.preventDefault();
+	  	console.log('add to likes');
+	  	User.addToLikes();
+	  	View.showRandomProfile();
+	  })
+
+	  $('#left_panel').on('click', '#no_button', function(event){
+	  	event.preventDefault();
+	  	console.log('add to DISlikes');
+	  	User.addToDislikes();
+	  	View.showRandomProfile();
+	  })
 
 	},
   render: function(templateElement, object, parentElement) {
@@ -16,19 +33,17 @@ View = {
     Mustache.parse(template);
     var rendered = Mustache.render(template, object);
     parentElement.append(rendered);
-  }
-	
-}
+  },
 
-User = {
-
-	// TEST: first thing shown is a random profile, with conditions: relevant gender, and not in the user's arrays, and not yourself
+  // FIRST: show is a random profile
+	// NEXT with conditions: relevant gender, and not in the user's arrays, and not yourself
 	showRandomProfile: function() { 
 		$.get('/users', function(response) {
-			// get random integer between 0 and last index of response (array with ALL profiles)
+			// get random index in the response.length (response = array with ALL profiles)
 			var i = getRandomInt(0, response.length - 1);
-			// show response[i]
 			console.log(response[i]);
+			// clear the profile box before adding the new one
+			$('#left_panel').empty();
 			View.render($('#random_profile_template'), response[i], $('#left_panel'));
 		})
 	},
@@ -36,6 +51,32 @@ User = {
 	// TEST: first show ALL matches, as user didnt choose a mood
 	showMatches: function() {
 		// code here
+	}
+	
+}
+
+// ****** USER FUNCTIONS *****
+
+User = {
+
+	addToLikes: function() {
+		// add to current user array of likes
+		// emit socket LIKE to server
+		// chechIfMatch();
+	},
+
+	chechIfMatch: function() {
+		// IF match
+		// showAnimationMatch,
+		// add to Match array, 
+		// emit socket Match, 
+		// and add to Panel of Matches
+		// ELSE nothing
+	},
+
+	addToDislikes: function() {
+		// add to Dislike array
+		// emit socket DISLIKE
 	}
 }
 
@@ -52,7 +93,7 @@ $(document).ready(function() {
 	View.eventListeners();
 
 	// AS MVP we TEST with showing randome profile and ALL matches
-	User.showRandomProfile();
-	User.showMatches
+	View.showRandomProfile();
+	View.showMatches();
 
 })
