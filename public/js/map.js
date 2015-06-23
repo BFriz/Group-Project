a56864b586fd84ac6b0151ec3dc01bf00b01bebd
Christopher Haines <hainesy91@gmail.com>
2015-06-23 11:56:00 +0100

    adding map to load after page loads function

--- public/js/map.js
+++ public/js/map.js
@@ -1,18 +1,20 @@
 //Initialize function for Google Maps
-var moodMap
-function initialize() {
-  map = new google.maps.Map(document.getElementById('googleMap'), {
-    zoom: 12,
-    center: {lat: 51.507351, lng: -0.127758}
-  });
-}
-//Event listener for the map to load
-google.maps.event.addDomListener(window, 'load', initialize);
-
 function initialize() {
   var mapOptions = {
     zoom: 12,
     center: new.google.maps.LatLng(51.507351, -0.127758)
   };
-  var 
+  var map = new google.maps.Map(document.getElementById('googleMap'),
+    mapOptions);
+}
+
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +
+      '&signed_in=true&callback=initialize';
+  document.body.appendChild(script);
+}
+
+window.onload = loadScript;
 }
