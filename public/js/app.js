e0593cf383b763c51d2efd6969c80d08af3a5dc6
Christopher Haines <hainesy91@gmail.com>
2015-06-23 08:56:13 +0100

    test code for first jquery request

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,8 @@
 // code in here for the code, ajax requests, etc
-$(document).ready() {
-  
-}
+$(document).ready() 
+  $( "#mood_menu a" ).click(function(event) {
+    console.log("button clicked!")
+    event.preventDefault();
+    $(".flirty")
+    alert( "Handler for .click() called." );
+  });
