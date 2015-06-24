8cb5619f105eb007e7d1e832736af2fd16cffae2
Ben Frisby <bfrisb@gmail.com>
2015-06-24 17:36:07 +0100

    finished seeding to match the input from Facebook

--- app/models/seeds.js
+++ app/models/seeds.js
@@ -19,19 +19,21 @@ User.collection.dropIndex('password_1');
 // James likes Mathilda, but no match; James doesnt like Lauren 
 
 User.create({
-	display_name: 'Mathilda',
-	dob: '19/12/1988',
+	email: "MyNameIsMathilda@gmail.com",
+	name: 'Mathilda',
 	gender: 'female',
+	profile_pic_url: "http://media.repro-mayr.de/04/73304.jpg"
 	mood: 'party',
 	location: 'E1 5JL'
 }, function(err, mathilda) {
 	if (err) { console.log(err) }
-	else { console.log('mathilda creaetd');}
+	else { console.log('mathilda created');}
 	// James likes Mathilda
 	User.create({
-		display_name: 'James',
-		dob: '19/12/1984',
+		email: 'SmellyHead@gmail.com',
+		name: 'James',
 		gender: 'male',
+		profile_pic_url: "http://img1.wikia.nocookie.net/__cb20081205183737/desencyclopedie/images/4/4b/Geek.jpg"
 		mood: 'party',
 		location: 'E1 6BX'
 	}, function(err, james) {
@@ -40,26 +42,30 @@ User.create({
 		console.log('this is James likes ', james.likes);
 	// James doesnt like Lauren
 		User.create({
-			display_name: 'Lauren',
-			dob: '19/10/1988',
+			email: "lookatmeiamlauren@gmail.com",
+			name: 'Lauren',
 			gender: 'female',
+			profile_pic_url: 'http://msutoday.msu.edu/_/img/assets/2013/deborah-feltz.jpg',
 			mood: 'chatty',
 			location: 'SW8 1SP'
 		}, function(err, lauren) {
 				james.dislikes.push(lauren._id);
 				console.log('James dislikes lauren, here are his dislikes ', james.dislikes);
 				User.create({
-					display_name: 'Jeremy',
-					dob: '19/10/1980',
+					email: 'Jdawgyall@gmail.com',
+					name: 'Jeremy',
 					gender: 'male',
+					profile_pic_url: ''
 					mood: 'surprise me',
+
 					location: 'W1D 4UR'
 				}, function(err, jeremy) {
 					if (!err) console.log ('jeremy created');
 					User.create({
-						display_name: 'Lexie',
-						dob: '19/07/1988',
+						email: 'lalalala@gmail.com',
+						name: 'Lexie',
 						gender: 'female',
+						profile_pic_url: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAORAAAAJDJjOTkxM2RmLTljYTMtNGVhOS04NjVmLTVmODJiNTY5YmI0Ng.jpg',
 						mood: 'surprise me',
 						location: 'EC4N 5AW'
 					}, function(err, lexie) {
