dd3188a1bb123171b55984d0dfb96aba48a4ce40
Christopher Haines <hainesy91@gmail.com>
2015-06-25 16:12:18 +0100

    jeremy updated the Gmap function so we can add value to the markers

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,11 @@
 //Variables that will be updated by the functions.
 var map;
+var marker;
 var geocoder;
 var locations = [];
+var all_users;
+//var locations = {};
+
 
 //Initialize function for Google Maps
 function initialize() {
@@ -11,7 +15,7 @@ function initialize() {
     center: latlng
   }
   map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
-  var marker = new google.maps.Marker({
+  marker = new google.maps.Marker({
     position: latlng,
     map: map,
   });
@@ -20,37 +24,104 @@ function initialize() {
 //Function for obtaining the locations 
 function listLocation() {
     $.get('/map', function(response) {
+      // also return user name from database
       console.log(response);
-        $.each(response, function(index, user) {
-          locations.push(user.location);
-        });
-    }).done(function(){
-      addMarkers();
+      $.each(response, function(index, user) {
+        locations.push(user.location);
+        all_users = response;
+        //locations.push([user.location, user.name]);
+        // you~ll need to push an array with two elements
+        // then you want to re'write addmarker to get each postcode
+
+      });
+      return all_users;
+    })
+    .done(function(all_users){
+      addMarkers(all_users, function(coords) {
+        var usersAndCoordinates = _.zip(all_users, coords);
+
+        for (var i = 0; i < usersAndCoordinates.length; i++) {
+          var marker = new google.maps.Marker({
+            //icon: usersAndCoordinates[i][0].facebook.profile_pic_url,
+            position: usersAndCoordinates[i][1],
+            map: map,
+            html: '<h1>' + usersAndCoordinates[i][0].facebook.name + '</h1>' 
+          });
+
+          var infoWindow = new google.maps.InfoWindow( { content: "Loading content..." } );
+
+          google.maps.event.addListener( marker, 'click', function() {
+            infoWindow.setContent(this.html);
+            infoWindow.open(map, this);
+          });
+        };
+      });
     });
 }
 
 //Function for adding markers to the page based on the locations from the database.
-function addMarkers() {
+function addMarkers(all_users, callback) {
+  var coords = [];
+  var users = all_users;
   geocoder = new google.maps.Geocoder();
-    for (i = 0; i < locations.length; i++) {
-      var address = locations[i];
-      geocoder.geocode( {'address': address}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          var marker = new google.maps.Marker({
-          position: results[0].geometry.location,
-          map: map,
-          });
-        }  
-      });
-    }
+
+  for (var i = 0; i < users.length; i++) {
+     // var windowContent = '<h1>'+ locations(i)(1)) +'</h1>';
+      // address will then be locations(i)(0)
+      // name will then be locations(i)(1)
+    var address = users[i].location;
+    var windowContent = '<h1>' + users[i].facebook.name + '</h1>';
+      console.log('window contente', windowContent);
+
+      console.log(users[i]); // WORKS
+
+    geocoder.geocode( {'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        console.log(users);  // UNDEFINED
+        console.log(windowContent);
+
+        coords.push(results[0].geometry.location);
+
+        if(coords.length === users.length) {
+          callback(coords);
+        }
+      }
+    });
+  }
 }
+
+
+
+
+
+
+
+
+
+
+          // var marker = new google.maps.Marker({
+          //   // icon: users[i].facebook.profile_pic_url,
+          //   position: results[0].geometry.location,
+          //   map: map,
+          //   html: windowContent  // ALWAYS CHRIS HAINES
+          // });
+
+
+
+          // infoWindow = new google.maps.InfoWindow( { content: "Loading content..." } );
+
+          // google.maps.event.addListener( marker, 'click', function() {
+          //   infoWindow.setContent(this.html);
+          //   console.log('marker is', marker);
+          //   infoWindow.open(map, this);
+          // });
 
 // var profileContent ='<div id="content">' +
 //   '<div id="siteNotice">' + 
 //   '</div>' + 
 //   '<h1 id="userName" class="userName">Mathilda</h1>' + 
 //   '<div id="bodyContent">' +
-//   '<p>Mathilda is single and feeling flirty!</p>' +
+//   '<p>Current Mood: </p>' +
 //   '</div>' +
 //   '</div>';
 
@@ -63,10 +134,11 @@ function addMarkers() {
 $(document).ready(function() {
   google.maps.event.addDomListener(window, 'load', initialize);
   listLocation();
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.open(map,marker);
-  });
+
+
+  
 });
+
 
 
 
