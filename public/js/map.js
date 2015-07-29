bd3436f8bee0a4b3e1fe7f4e3551d143c875f259
Guillaume Perrin <gperrin01@gmail.com>
2015-07-29 19:56:25 +0100

    styling the map a bit

--- public/js/map.js
+++ public/js/map.js
@@ -9,16 +9,25 @@ var all_users;
 
 //Initialize function for Google Maps
 function initialize() {
+
   var latlng = new google.maps.LatLng(51.50722, -0.12750);
+  var snazzyMap = [{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"color":"#f7f1df"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#d0e3b4"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"geometry","stylers":[{"color":"#fbd3da"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#bde6ab"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffe15f"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efd151"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"black"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#cfb2db"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#a2daf2"}]}]
+
   var mapOptions = {
-    zoom: 12,
-    center: latlng
-  }
+      zoom: 12,
+      center: latlng,
+      mapTypeControl: false,
+      panControl: false,
+      styles: snazzyMap
+    };
+
   map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+
   marker = new google.maps.Marker({
     position: latlng,
     map: map,
   });
+
 }
 
 //Function for obtaining the locations 
