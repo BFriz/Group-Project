ed56a517671d5ae60515e9ca5243586841118a7a
Guillaume Perrin <gperrin01@gmail.com>
2015-08-03 11:28:20 +0100

    special marker for current user plus picture now shoing on infowindow

--- public/js/map.js
+++ public/js/map.js
@@ -26,7 +26,9 @@ Map = {
  //Initialize function for Google Maps
  initialize: function() {
 
-    Map.latlng = new google.maps.LatLng(51.50722, -0.12750);
+    Map.latlng = new google.maps.LatLng(51.50722, -0.12750); // LONDON by default for the moment
+    Map.currUserIcon = "../img/red_marker.png";
+
     var snazzyMap = [{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"color":"#f7f1df"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#d0e3b4"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"geometry","stylers":[{"color":"#fbd3da"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#bde6ab"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffe15f"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efd151"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"black"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#cfb2db"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#a2daf2"}]}]
 
     var mapOptions = {
@@ -42,11 +44,6 @@ Map = {
 
     Map.map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
 
-    Map.marker = new google.maps.Marker({
-      position: Map.latlng,
-      map: Map.map,
-    });
-
     // now show all users on map
     Map.listLocations();
 
@@ -61,26 +58,37 @@ Map = {
       Map.usersLocations.push(user.location);
     });
 
-    // callback function to run on Map.addMarkers
+    // callback function to run on Map.addMarkers, see definition below for explanations
+    // NOTE underscore _.after() function seems like an easier choice
     Map.addMarkers(AllUsers, function(coords){
       console.log('adding markers');
+      // add markers will push all users' coords in an array which we 'zip' with all users
       var usersAndCoordinates = _.zip(AllUsers, coords);
 
       for (var i = 0; i < usersAndCoordinates.length; i++) {
+
         var name = usersAndCoordinates[i][0].facebook.name;
         var mood = usersAndCoordinates[i][0].mood;
         var pic = usersAndCoordinates[i][0].facebook.profile_pic_url;
         var id = usersAndCoordinates[i][0]._id;
         var position = usersAndCoordinates[i][1];
+       
+        // Click on a marker will show the user info on the infowindow
+        var html = "<div class='mini_marker_info' data-id='" +id + "'>";
+        html += "<img src='" +pic+ "' class='img-responsive img-circle'><p>" +name+ " <span class='glyphicon glyphicon-star-empty'></span> ";
+        html += mood + "</p></div>";
 
         var marker = new google.maps.Marker({
-          position: position ,
+          position: position,
           map: Map.map,
           draggable: true,
-          html: '<div class="mini_marker_info" data-pic="' + pic + '" data-id="' + id + '"><p>' + name + '</p> <p>' + mood + '</p></div>' 
+          html: html
         });
-        var infoWindow = new google.maps.InfoWindow();
+        // special icon for current user
+        if (id === StorageUser._id) { marker.setIcon(Map.currUserIcon)}
 
+        // infowindow on click
+        var infoWindow = new google.maps.InfoWindow();
         google.maps.event.addListener(marker, 'click', function() {
           infoWindow.setContent(this.html);
           infoWindow.open(Map.map, this);
@@ -91,6 +99,9 @@ Map = {
   },
 
 //Function for adding markers to the page based on the locations from the database.
+// as there is an external call to gglmap to retrieve the coords of the marker,
+// we trigger a callback function when the 'google work' is done
+// NOTE underscore _.after() function seems like an easier choice
   addMarkers: function(users, callback) {
     var coords = [];
     Map.geocoder = new google.maps.Geocoder();
@@ -103,11 +114,9 @@ Map = {
       var windowContent = '<p>' + users[i].facebook.name + '</p>';
 
       Map.geocoder.geocode( {'address': address}, function(results, status) {
-        console.log('status', status );
         if (status == google.maps.GeocoderStatus.OK) {
           coords.push(results[0].geometry.location);
         }
-
         if (i === users.length) {
           callback(coords);
         }
